7badab0b3f62cca352da2dedcb7cbca718c31943
etch <etchons1@gmail.com>
2023-10-17 12:49:29 -0500

    did relativity for pos.x

--- button1.gd
+++ button1.gd
@@ -5,7 +5,7 @@ extends Button
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.y = $"../button2".position.y
-	#size and position.x of buttons still needs to be relative
+	position.x = (($"../bar".position.x+((300.0/648.0)*get_viewport_rect().size.x))/5)*2
 
 
 func _on_pressed():
--- button2.gd
+++ button2.gd
@@ -4,7 +4,7 @@ extends Button
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position.y = $"../bar".position.y + ((10.0/1152)*get_viewport_rect().size.y)
-	
+	position.x = ($"../bar".position.x+((50.0/648.0)*get_viewport_rect().size.x))/5
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
