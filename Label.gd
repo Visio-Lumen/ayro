feb60f52fd26166f80b8c425ac8b6e6a13a5c061
etch <etchons1@gmail.com>
2023-10-17 13:28:59 -0500

    trying

--- Label.gd
+++ Label.gd
@@ -3,7 +3,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2.ZERO
+	print(position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- button1.gd
+++ button1.gd
@@ -2,7 +2,6 @@ extends Button
 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.y = $"../button2".position.y
 	position.x = (($"../bar".position.x+((300.0/648.0)*get_viewport_rect().size.x))/5)*2
@@ -11,5 +10,3 @@ func _process(delta):
 
 func _on_pressed():
 	get_tree().change_scene_to_file("res://timer.tscn")
-	
-
