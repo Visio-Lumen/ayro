9f449dbe39047c3bdbdc9ce36b58ca2d1fcb07dd
etch <etchons1@gmail.com>
2023-10-17 12:54:21 -0500

    EVERYTHING IS RELATIVE FINALLY
    IM DONE WITH MATH FOREVER MUAHAHAHHAHAHAHA

--- button1.gd
+++ button1.gd
@@ -6,6 +6,7 @@ extends Button
 func _process(delta):
 	position.y = $"../button2".position.y
 	position.x = (($"../bar".position.x+((300.0/648.0)*get_viewport_rect().size.x))/5)*2
+	size = $"../button2".size
 
 
 func _on_pressed():
--- button2.gd
+++ button2.gd
@@ -5,6 +5,7 @@ extends Button
 func _ready():
 	position.y = $"../bar".position.y + ((10.0/1152)*get_viewport_rect().size.y)
 	position.x = ($"../bar".position.x+((50.0/648.0)*get_viewport_rect().size.x))/5
+	size = Vector2((87.0/1152.0)*get_viewport_rect().size.y,(55.0/1152.0)*get_viewport_rect().size.y)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
